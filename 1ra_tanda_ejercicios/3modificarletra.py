213b48c699caa5ab464ecddf64e5e0e05de41836
Moises Alejandro Montiel Briceño <74522304+MAMB1999@users.noreply.github.com>
2021-12-08 22:24:45 -0300

    Eliminados archivos subidos por error

--- 1ra_tanda_ejercicios/3modificarletra.py
+++
@@ -1,7 +0,0 @@
-def modificarLetra(index,palabra,palabraCambia):
-        """Funcion que permite el cambio en una cadena de texto mediante el ingreso de un string,\
-             indice y la pabra a cambiar"""
-        palabra = list(palabra)
-        palabra[index] = palabraCambia
-        print("".join(palabra)) 
-
